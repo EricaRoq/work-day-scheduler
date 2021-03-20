0b633356c7b3df137cda16ee1517c4d0b0646788
Erica Roquemore <enroquemore@gmail.com>
2021-03-20 12:54:15 -0400

    changed folders

--- develop/script.js
+++ develop/script.js
@@ -1,7 +1,15 @@
 
 // Date and Time set up
-moment().format('MMMM Do YYYY, h:mm:ss a');
-    
+// moment().format('MMMM Do YYYY, h:mm:ss a');
+
+var numberOfButtons = document.querySelectorAll(".saveBtn").length;
+
+for (var i = 0; i < numberOfButtons; i++) {
+    document.querySelectorAll("button .saveBtn")[i].addEventListener("click", function () {
+        alert("I got clicked!");
+        
+    });
+}
 
 
 
