2eba79e7f833c33f1e2576f24d3751d61abea8f6
Erica Roquemore <enroquemore@gmail.com>
2021-03-20 17:30:08 -0400

    added event listeners to save buttons

--- develop/script.js
+++ develop/script.js
@@ -1,19 +1,49 @@
-
-// Date and Time set up
-// moment().format('MMMM Do YYYY, h:mm:ss a');
-
-var numberOfButtons = document.querySelectorAll(".saveBtn").length;
-
-for (var i = 0; i < numberOfButtons; i++) {
-    document.querySelectorAll("button .saveBtn")[i].addEventListener("click", function () {
-        alert("I got clicked!");
-        
-    });
-}
+var hourNine = document.querySelector("#hour-nine .saveBtn");
+var hourTen = document.querySelector("#hour-ten .saveBtn");
+var hourEleven = document.querySelector("#hour-eleven .saveBtn");
+var hourTwelve = document.querySelector("#hour-twelve .saveBtn");
+var hourOne = document.querySelector("#hour-one .saveBtn");
+var hourTwo = document.querySelector("#hour-two .saveBtn");
+var hourThree = document.querySelector("#hour-three .saveBtn");
+var hourFour = document.querySelector("#hour-four .saveBtn");
+var hourFive = document.querySelector("#hour-five .saveBtn");
 
 
+hourNine.addEventListener("click", function () {
+    alert("I got clicked!");
+});
 
+hourTen.addEventListener("click", function () {
+    alert("I got clicked!");
+});
 
+hourEleven.addEventListener("click", function () {
+    alert("I got clicked!");
+});
+
+hourTwelve.addEventListener("click", function () {
+    alert("I got clicked!");
+});
+
+hourOne.addEventListener("click", function () {
+    alert("I got clicked!");
+});
+
+hourTwo.addEventListener("click", function () {
+    alert("I got clicked!");
+});
+
+hourThree.addEventListener("click", function () {
+    alert("I got clicked!");
+});
+
+hourFour.addEventListener("click", function () {
+    alert("I got clicked!");
+});
+
+hourFive.addEventListener("click", function () {
+    alert("I got clicked!");
+});
 
     
 
